ec490b46244de84cec01704b072e29a2422f7035
John Kirch <john.kirch.business@gmail.com>
2022-04-21 09:03:30 -0400

    creating the google strategy and callbacks

--- bootstrap-logins/routes/api/authRoutes.js
+++ bootstrap-logins/routes/api/authRoutes.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../../config/keys');
 
-router.get('/google', (req, res) => {
-  res.send({ page: 'Google Auth Page' });
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, profile, email) => {
+      console.log(accessToken);
+      console.log(profile);
+      console.log(email);
+    }
+  )
+);
+
+router.get('/login', (req, res) => {
+  res.render('login');
 });
+
+router.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+router.get('/google/callback', passport.authenticate('google'), (req, res) =>
+  res.send(res)
+);
 
 module.exports = router;
