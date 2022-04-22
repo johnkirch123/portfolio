12345079b0e60fdd10a454be28a3db2c92ad58e5
John Kirch <john.kirch.business@gmail.com>
2022-04-22 13:07:10 -0400

    proxy and config changes

---
+++ bootstrap-logins/config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  twitterClientID: process.env.TWITTER_CLIENT_ID,
+  twitterClientSecret: process.env.TWITTER_CLIENT_SECRET
+};
--- bootstrap-logins/index.js
+++ bootstrap-logins/index.js
@@ -19,9 +19,16 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/', (req, res) => res.send({ page: 'Home' }));
-
 app.use('/auth', require('./routes/api/authRoutes'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
