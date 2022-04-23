a0d61292c5d2025af0c41f0ad66c0f627a9d284f
John Kirch <john.kirch.business@gmail.com>
2022-04-23 10:09:56 -0400

    map and list functionality started

--- travel-advisor/src/App.js
+++ travel-advisor/src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+import PlaceDetails from './components/PlaceDetails/PlaceDetails';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hi there</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
