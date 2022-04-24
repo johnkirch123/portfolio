d3524fac20ff1137fb8a3f956cc400eda1fdcc09
John Kirch <john.kirch.business@gmail.com>
2022-04-24 11:09:35 -0400

    reworking the entire portfolio page

--- john-kirch/routes/api/projects.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/test", (req, res) => {
-  res.send("You have reached the test page for Projects");
-});
-
-module.exports = router;
