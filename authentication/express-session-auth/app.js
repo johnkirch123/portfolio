989bbcc42b93ee59a8fd2f96ff600c90d03a9ceb
John Kirch <john.kirch.business@gmail.com>
2022-06-22 12:59:20 -0400

    express-session auth implementation

--- authentication/express-session-auth/app.js
+++ authentication/express-session-auth/app.js
@@ -53,6 +53,12 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use((req, res, next) => {
+  console.log(req.session);
+  console.log(req.user);
+  next();
+});
+
 /**
  * -------------- ROUTES ----------------
  */
